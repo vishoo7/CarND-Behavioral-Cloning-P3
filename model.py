050d9fa5a7e487f7dc8b1ee1e2efdb3d64d06864
Vishal <vishal_agrawal@ymail.com>
2017-03-10 21:44:24 -0500

    3 epochs + added 5 images (left, right, and flipped for all)

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 
 samples = []
-with open('./driving_log.csv') as csvfile:
+with open('./data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         samples.append(line)
@@ -19,24 +19,48 @@ with open('./driving_log.csv') as csvfile:
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
+#tunable parameter to adjust left/right steering angles
+correction = 0.2
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
-        for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
+        for offset in range(0, num_samples, int(batch_size/6)): #batch_size/6 since we have six images
+            batch_samples = samples[offset:offset+int(batch_size/6)]
 
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = './IMG/'+batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(name)
-                center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
+                center_name = './data/IMG/'+batch_sample[0].split('/')[-1]
+                left_name = './data/IMG/'+batch_sample[1].split('/')[-1]
+                right_name = './data/IMG/'+batch_sample[2].split('/')[-1]
 
-            # trim image to only see section with road
+                names = [center_name, left_name, right_name]
+                try: 
+                    steering_center = float(batch_sample[3])
+                    steering_left = steering_center + correction
+                    steering_right = steering_center - correction
+                except ValueError:
+                    continue
+
+                for i, name in names:
+                    image = cv2.imread(name)
+                    images.append(image)
+                    if i==0:
+                        steering = steering_center
+                    elif i==1:
+                        steering = steering_left
+                    elif i==2:
+                        steering = steering_right
+                    angles.append(steering)
+
+					#Add flipped image
+                    image_flipped = np.fliplr(image)
+                    angle_flipped = -steering
+                    images.append(image_flipped)
+                    angles.append(angle_flipped)
+
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
@@ -44,8 +68,6 @@ def generator(samples, batch_size=32):
 # compile and train the model using the generator function
 train_generator = generator(train_samples, batch_size=128)
 validation_generator = generator(validation_samples, batch_size=128)
-
-ch, row, col = 3, 80, 320  # Trimmed image format
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5,
@@ -63,8 +85,8 @@ model.add(Dense(10))
 model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 history_object = model.fit_generator(train_generator, samples_per_epoch= \
-	len(train_samples), validation_data=validation_generator, \
-	nb_val_samples=len(validation_samples), nb_epoch=5, verbose=1)
+	len(train_samples)*6, validation_data=validation_generator, \
+	nb_val_samples=len(validation_samples), nb_epoch=3, verbose=1)
 
 model.save('model.h5')  # creates a HDF5 file 'model.h5'
 
